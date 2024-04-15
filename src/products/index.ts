57e5024c4392eb39223c889d94ac39b3b2a5f609
emimontanari <emimontanari@icloud.com>
2024-04-15 16:03:22 -0300

    React Query Store Finalizado

---
+++ src/products/index.ts
@@ -0,0 +1,20 @@
+export { usePrefetchProduct } from './hooks/usePrefetchProduct';
+export { useProducts } from './hooks/useProducts';
+export { useProduct } from './hooks/useProduct';
+export { useProductMutation } from './hooks/useProductMutation';
+export { productsApi } from './api/productsApi';
+
+export { ProductCard } from './components/ProductCard';
+export { ProductList } from './components/ProductList';
+export type { Product } from './interfaces/product';
+
+export { StoreLayout } from './layout/StoreLayout';
+
+
+export { CompleteListPage } from './pages/CompleteListPage';
+export { MensPage } from './pages/MensPage';
+export { NewProduct } from './pages/NewProduct';
+export { WomensPage } from './pages/WomensPage';
+export { ProductById } from './pages/ProductById'
+
+export * as productActions from './services/actions'
---
+++ src/products/pages/MensPage.tsx
@@ -0,0 +1,18 @@
+import { ProductList, useProducts } from ".."
+
+export const MensPage = () => {
+
+  const {isLoading, products} = useProducts({
+    filterKey: "men's clothing"
+  })
+  return (
+    <div className="flex-col">
+      <h1 className="text-2xl font-bold">Productos para hombres</h1>
+      {
+        isLoading && <p>Cargandoo...</p>
+      }
+      <ProductList products={products}/>
+
+    </div>
+  )
+}
